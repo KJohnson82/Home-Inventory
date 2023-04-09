8d8db998b4209db3deb47fa53b2c18f1b3e6f85f
KJohnson82 <rojohnson@gmail.com>
2023-04-08 23:52:13 -0500

    Its finally working, mostly. Now to adjust some more code, style it, and enjoy it.

--- lib/Homes.dart
+++ lib/Homes.dart
@@ -1,216 +1,140 @@
-import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'ItemsForm.dart';
 import 'Rooms.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'main.dart';
 
+
 class Home {
-  int? homeId;
   String? homeName;
   Map<Object, Room>? rooms;
 
-  Home({this.homeId, this.homeName, Map<int, Room>? rooms}) {
+  Home({this.homeName, Map<int, Room>? rooms}) {
     this.rooms = rooms ?? {};
   }
 }
 
-class HomeController {
-  DatabaseReference homesRef = FirebaseDatabase.instance.ref('homes');
-  var homes = <String, Home>{};
-  var highestId = 0;
-  // var homes = <int, Home>{};
-
-  void addHome(String homeName) {
-    if (homes.length < 3) {
-      int newId = (highestId + 1);
-      // homes[newId] = Home(homeId: newId, homeName: homeName, rooms: {});
-      final newHomeRef = FirebaseDatabase.instance.ref().child('homes').push();
-      final newHomeKey = newHomeRef.key;
-      highestId = newId;
-      // FirebaseDatabase.instance.ref().child('homes').push().set({
-      //   'homeId': homes[newId]?.homeId,
-      //   'homeName': homes[newId]?.homeName,
-      //   // 'homeRooms': homes[newId]?.rooms,
-      // });
-      if (newHomeKey != null) {
-        homes[newHomeKey] =
-            Home(homeId: homes.length + 1, homeName: homeName, rooms: {});
-        newHomeRef.set({
-          'homeId': homes[newHomeKey]?.homeId,
-          'homeName': homes[newHomeKey]?.homeName,
-          // 'homeRooms': homes[newHomeKey]?.rooms,
-        });
-        print(homes[newId]?.homeId);
-        print(homes[newId]?.homeName);
-        print(homes[newId]?.rooms);
-      }
-    }
-  }
-
-  Future<void> getHomes() async {
-    DataSnapshot snapshot = await homesRef.get();
-    if (snapshot.value != null) {
-      Map<dynamic, dynamic> data = snapshot.value as Map<dynamic, dynamic>;
-      homes.clear();
-
-      print('FB Data: $data');
-      for (var key in data.keys) {
-        Map<dynamic, dynamic>? homeData = data[key] as Map<dynamic, dynamic>;
-        int? homeId = homeData['homeId'];
-        if (homeId == null) {
-          continue;
-        }
-        String homeName = homeData['homeName'];
-
-        homes[key] = Home(
-          homeId: homeId,
-          homeName: homeName,
-          rooms: {},
-        );
-        if (homeData['rooms'] != null) {
-          Map<dynamic, dynamic> roomDataMap =
-              homeData['rooms'] as Map<dynamic, dynamic>;
-          for (var roomKey in roomDataMap.keys) {
-            Map<dynamic, dynamic> roomData =
-                roomDataMap[roomKey] as Map<dynamic, dynamic>;
-            int roomId = roomData['roomId'];
-            String roomDesc = roomData['roomName'];
-            // You can modify this to retrieve items if needed
-            Map<Object, Item>? items = {};
-
-            homes[homeId]!.rooms![roomId] =
-                Room(roomId: roomId, roomDesc: roomDesc, items: items);
-          }
-        }
-      }
-    }
-  }
-}
-
 class HomesPage extends StatefulWidget {
-  HomesPage({Key? key}) : super(key: key);
+  const HomesPage({super.key});
 
   @override
   _HomesPageState createState() => _HomesPageState();
 }
 
 class _HomesPageState extends State<HomesPage> {
-  final TextEditingController _homeName = TextEditingController();
+  final _homes = db.collection("homes");
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _homeNameController = TextEditingController();
   final FocusNode _focusNode = FocusNode();
-  final HomeController controller = HomeController();
 
-  @override
-  void initState() {
-    super.initState();
-    controller.getHomes().then((value) {
-      print('Homes fetched: ${controller.homes}');
-      setState(() {});
-    });
+  void _addHome() async {
+    if (_formKey.currentState!.validate()) {
+      await _homes.add({
+        'homeName': _homeNameController.text,
+      });
+      _homeNameController.clear();
+      Navigator.of(context).pop();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.builder(
-        scrollDirection: Axis.vertical,
-        padding: const EdgeInsets.fromLTRB(100, 50, 100, 50),
-        itemCount: controller.homes.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 1,
-        ),
-        itemBuilder: (BuildContext context, int index) {
-          final homeId = controller.homes.keys.toList()[index];
-          final home = controller.homes[homeId];
-          print('Index: $index, HomeId: $homeId, Home: $home');
-          return InkWell(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => RoomsPage(
-                            homeName: home?.homeName ?? '',
-                          ),
-                      settings: RouteSettings(
-                        arguments: home?.homeName ?? '',
-                      )));
-              print('This was pressed ${home?.homeName}');
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.amber,
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: const [
-                    BoxShadow(
-                        color: Colors.black12,
-                        spreadRadius: 1,
-                        blurRadius: 10,
-                        offset: Offset(2, 6))
-                  ],
-                  shape: BoxShape.rectangle,
-                ),
-                child: Text(home?.homeName ?? ''),
-              ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _homes.snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final homes = snapshot.data!.docs;
+
+          return GridView.builder(
+            scrollDirection: Axis.vertical,
+            padding: const EdgeInsets.fromLTRB(100, 50, 100, 50),
+            itemCount: homes.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              // childAspectRatio: 2,
             ),
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RoomsPage(
+                          homeId: homes[index].id,
+                          homeName: homes[index]['homeName'],
+                        ),
+                      ));
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.black12,
+                            spreadRadius: 1,
+                            blurRadius: 10,
+                            offset: Offset(2, 6))
+                      ],
+                      shape: BoxShape.rectangle,
+                    ),
+                    child: Text(homes[index]['homeName']),
+                  ),
+                ),
+              );
+            },
           );
         },
       ),
-      floatingActionButton: FloatingActionButton.large(
-        elevation: 4,
-        foregroundColor: Colors.white,
-        onPressed: controller.homes.length >= 3
-            ? null
-            : () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: const Text("Enter New Home Name: "),
-                      content: TextField(
-                        controller: _homeName,
-                        decoration:
-                            const InputDecoration(hintText: 'Home Name'),
-                        focusNode: _focusNode,
-                        autofocus: true,
-                        onSubmitted: (value) {
-                          setState(() {
-                            controller.addHome(_homeName.text);
-                          });
-                          _homeName.clear();
-                          Navigator.pop(context);
-                        },
-                        onTap: () {
-                          _focusNode.requestFocus();
-                        },
-                      ),
-                      actions: [
-                        ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                controller.addHome(_homeName.text);
-                              });
-                              _homeName.clear();
-                              Navigator.pop(context);
-                              print(_homeName.text);
-                            },
-                            child: const Text("Save"))
-                      ],
-                    );
-                  },
-                );
-              },
-        child: const Icon(
-          Icons.add_home_work_outlined,
-          size: 60,
-        ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 2,
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Add A New Home'),
+                content: Form(
+                  key: _formKey,
+                  child: TextFormField(
+                    controller: _homeNameController,
+                    decoration: const InputDecoration(labelText: 'Home Name'),
+                    focusNode: _focusNode,
+                    autofocus: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a name';
+                      }
+                      return null;
+                    },
+                    onEditingComplete: () {
+                      _addHome();
+                      _homeNameController.clear();
+                    },
+                  ),
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('Save'),
+                    onPressed: () {
+                      _addHome();
+                      _homeNameController.clear();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add_home_outlined),
       ),
     );
   }
 }
-
-// void main() {
-//   runApp(HomesPage());
-// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,79 @@
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'Homes.dart';
+// import 'package:firebase_database/firebase_database.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
+//
+// import 'RoomItems.dart';
+// import 'Rooms.dart';
+//
+// var homes = <int, Home>{};
+//
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//
+//   FirebaseDatabase database = FirebaseDatabase.instance;
+//   DatabaseReference ref = FirebaseDatabase.instance.ref();
+//
+//   // DatabaseReference homesRef = FirebaseDatabase.instance.ref('homes');
+//
+//   runApp(
+//     MaterialApp(
+//       title: 'Material App',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('HOMEVENTORY'),
+//           centerTitle: true,
+//         ),
+//         body: HomesPage(),
+//       ),
+//     ),
+//   );
+// }
+//
+// // class MyApp extends StatelessWidget {
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return MaterialApp(
+// //       title: 'HOMEVENTORY',
+// //       home: Scaffold(
+// //         appBar: AppBar(
+// //           title: Text('HOMEVENTORY'),
+// //         ),
+// //         body: HomesPage(),
+// //       ),
+// //     );
+// //   }
+// // }
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'Homes.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
-import 'RoomItems.dart';
-import 'Rooms.dart';
-
-var homes = <int, Home>{};
+// var homes = <int, Home>{};
+FirebaseFirestore db = FirebaseFirestore.instance;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    // options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  FirebaseDatabase database = FirebaseDatabase.instance;
-  DatabaseReference ref = FirebaseDatabase.instance.ref();
-
-  // DatabaseReference homesRef = FirebaseDatabase.instance.ref('homes');
+  // FirebaseFirestore firestore = FirebaseFirestore.instance;
+  // FirebaseFirestore db = FirebaseFirestore.instance;
+  // CollectionReference homesRef = FirebaseFirestore.instance.collection('homes');
 
   runApp(
     MaterialApp(
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('HOMEVENTORY'),
+          title: const Text('HOMEVENTORY'),
           centerTitle: true,
         ),
         body: HomesPage(),
@@ -34,18 +81,3 @@ Future<void> main() async {
     ),
   );
 }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'HOMEVENTORY',
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('HOMEVENTORY'),
-//         ),
-//         body: HomesPage(),
-//       ),
-//     );
-//   }
-// }
